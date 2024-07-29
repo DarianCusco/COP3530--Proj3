6c09a32f2ea0986b3f732a419f0ac55609cdd0df
Darian Cusco <cusco.d@gmail.com>
2024-07-29 15:08:43 -0400

    added new bin function and website files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let colorCategories = ["blue", "green", "yellow", "orange", "red"]
+
+
+document.getElementById('svgMap').addEventListener('load', function() {
+    const svgDoc = this.contentDocument;
+    if (svgDoc) {
+        const alabama = svgDoc.querySelector('.al');
+        if (alabama) {
+            alabama.style.fill = 'blue';
+        } else {
+            console.log('The path element for Alabama is not found in the SVG');
+        }
+    } else {
+        console.log('Failed to load the SVG document');
+    }
+});
