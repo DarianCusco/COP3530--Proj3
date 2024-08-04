15624d2fc39a6a987a746cc7d1b7d039925cdfa4
jqiu0601 <116924547+jqiu0601@users.noreply.github.com>
2024-08-04 18:45:19 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,176 +2,189 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <sstream>
 #include <map>
+#include "AlienEncounters.h"
 #include "bin.h"
-
+#include "HashTable.h"
 using namespace std;
 
-struct AlienEncounters {
-    string city;
-    string state;
-    string country;
-    string shape;
-    string dateSighted;
-
-    AlienEncounters(string city, string state, string country, string shape, string dateSighted) {
-        this->city = city;
-        this->state = state;
-        this->country = country;
-        this->shape = shape;
-        this->dateSighted = dateSighted;
-    };
-};
 void ReadFile(vector<AlienEncounters>& dataContainer) {
-    ifstream myData;
-    myData.open("../data/ufo_sightings.csv");
-    if (!myData.is_open()){cout << "file not properly opened" << endl;}
-    else {
+	ifstream myData;
+	//myData.open("../data/ufo_sightings.csv");
+	myData.open("/ufo_sightings.csv");
+	if (!myData.is_open()) { cout << "file not properly opened" << endl; }
+	else {
 
-        string _city;
-        string _state;
-        string _country;
-        string _shape;
+		string _city;
+		string _state;
+		string _country;
+		string _shape;
 
-        string _sightedYear;
-        string _sightedMonth;
-        string _sightedDay;
+		string _sightedYear;
+		string _sightedMonth;
+		string _sightedDay;
 
-        string throwaway;
+		string throwaway;
 
-        string line;
-        if (getline(myData, line)) {cout << "Header: " << line << endl;}
-        //Using 50,000 for testing porpuses
-        while (getline(myData, line)) { //Change to "while (getline(myData, line))" to store all data points
-            stringstream ss(line);
+		string line;
+		if (getline(myData, line)) { cout << "Header: " << line << endl; }
+		//Using 50,000 for testing porpuses
+		while (getline(myData, line)) { //Change to "while (getline(myData, line))" to store all data points
+			stringstream ss(line);
 
-            getline(ss, throwaway, '"');
-            getline(ss, _city, '"');  //stores city info
-            getline(ss, throwaway, '"');
-            getline(ss, _state, '"');  //stores state info
-            getline(ss, throwaway, '"');
-            getline(ss, _country, '"'); //stores country info
-            getline(ss, throwaway, '"');
-            getline(ss, _shape, '"'); //stores shape info
-            getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, _city, '"');  //stores city info
+			getline(ss, throwaway, '"');
+			getline(ss, _state, '"');  //stores state info
+			getline(ss, throwaway, '"');
+			getline(ss, _country, '"'); //stores country info
+			getline(ss, throwaway, '"');
+			getline(ss, _shape, '"'); //stores shape info
+			getline(ss, throwaway, '"');
 
-            //Skips uneeded lines
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
-            getline(ss, throwaway, '"');
+			//Skips uneeded lines
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
+			getline(ss, throwaway, '"');
 
-            getline(ss, _sightedYear, '"');  //stores sighted year
-            getline(ss, throwaway, '"');
-            getline(ss, _sightedMonth, '"');  //stores sighted month
-            getline(ss, throwaway, '"');
-            getline(ss, _sightedDay, '"');  //stores sighted day
+			getline(ss, _sightedYear, '"');  //stores sighted year
+			getline(ss, throwaway, '"');
+			getline(ss, _sightedMonth, '"');  //stores sighted month
+			getline(ss, throwaway, '"');
+			getline(ss, _sightedDay, '"');  //stores sighted day
 
-            string _sightedDate;
-            _sightedDate = _sightedMonth + "/" + _sightedDay + "/" + _sightedYear;  //Adds all calendar info into one variable
+			string _sightedDate;
+			_sightedDate = _sightedMonth + "/" + _sightedDay + "/" + _sightedYear;  //Adds all calendar info into one variable
 
-            AlienEncounters temp(_city, _state, _country, _shape, _sightedDate);
-            dataContainer.push_back(temp);
+			AlienEncounters temp(_city, _state, _country, _shape, _sightedDate);
+			dataContainer.push_back(temp);
 
-         }
-    }
+		}
+	}
 }
 
 void writeBins(vector<vector<pair<string, int>>>dataContainer) {
-    ofstream myNewData("../data/sorted_ufo_sightings.csv");
+	ofstream myNewData("../data/sorted_ufo_sightings_Bin.csv");
 
-     for (int i = 0; i < dataContainer.size(); i++) {
-        // cout << "Bins: " << binRanges[i].first << "-" << binRanges[i].second;
-        
-        for (auto j : dataContainer[i]) {
-            myNewData << j.first << " " << j.second  << ",";
-            }
-        myNewData << "\n";
-    }
+	for (int i = 0; i < dataContainer.size(); i++) {
+		// cout << "Bins: " << binRanges[i].first << "-" << binRanges[i].second;
+
+		for (auto j : dataContainer[i]) {
+			myNewData << j.first << " " << j.second << ",";
+		}
+		myNewData << "\n";
+	}
 }
+
 map<string, int> getCountryStats(vector<AlienEncounters>& dataContainer) {
-    map<string, int> freq;
-    for (auto dt : dataContainer) {
-        freq[dt.state]++;
-    }
-    return freq;
+	map<string, int> freq;
+	for (auto dt : dataContainer) {
+		freq[dt.state]++;
+	}
+	return freq;
 }
 
 void createJSONFile(string csvFile, string jsonFile) {
-    /*
-    What I expect JSON to look like
-    
-    "Group X": 
-    [
-        {"state": y "occurances:" z},
-        {"state": y "occurances:" z},
-        {"state": y "occurances:" z},
-        {"state": y "occurances:" z},
-        {"state": y "occurances:" z},
-    ],
-    */
-    map<int, vector<pair<string, int>>> dataGrouped;
-    int groupNumber = 1;
+	/*
+	What I expect JSON to look like
 
-    ifstream myData;
-    myData.open(csvFile); //opens updated csv file
+	"Group X":
+	[
+		{"state": y "occurances:" z},
+		{"state": y "occurances:" z},
+		{"state": y "occurances:" z},
+		{"state": y "occurances:" z},
+		{"state": y "occurances:" z},
+	],
+	*/
+	map<int, vector<pair<string, int>>> dataGrouped;
+	int groupNumber = 1;
 
-    if (!myData.is_open()) {cout << "data not loaded properly" << endl;}
-    else {
-        string line;
-        string stateData;
-        while(getline(myData, line)) {
-            stringstream ss(line);
+	ifstream myData;
+	myData.open(csvFile); //opens updated csv file
 
-            while(getline(ss, stateData, ',')) {
-                string state;
-                int stateOccurances;
-                stringstream lineStream(stateData);
-                lineStream >> state >> stateOccurances;
-                dataGrouped[(groupNumber)].push_back(make_pair(state, stateOccurances));
-            }
-            groupNumber++;
-        }
-    }
+	if (!myData.is_open()) { cout << "data not loaded properly" << endl; }
+	else {
+		string line;
+		string stateData;
+		while (getline(myData, line)) {
+			stringstream ss(line);
 
-    ofstream outFile("../data/" + jsonFile);
-    bool groupOne = true; //true if first group
-    outFile << "{\n";
-    for (auto group : dataGrouped) {
-        if (!groupOne) {outFile << ",\n";} //used to seperate groups
-        groupOne = false;
-        bool firstEntry = true; //true if first data point in group
-        outFile << "\"" <<"group: " << group.first << "\": [\n";
+			while (getline(ss, stateData, ',')) {
+				string state;
+				int stateOccurances;
+				stringstream lineStream(stateData);
+				lineStream >> state >> stateOccurances;
+				dataGrouped[(groupNumber)].push_back(make_pair(state, stateOccurances));
+			}
+			groupNumber++;
+		}
+	}
 
-        for (auto state : group.second) {
-            if (!firstEntry) {outFile << ",\n";} //Used to seperate entries
-            firstEntry = false;
+	ofstream outFile("../data/" + jsonFile);
+	bool groupOne = true; //true if first group
+	outFile << "{\n";
+	for (auto group : dataGrouped) {
+		if (!groupOne) { outFile << ",\n"; } //used to seperate groups
+		groupOne = false;
+		bool firstEntry = true; //true if first data point in group
+		outFile << "\"" << "group: " << group.first << "\": [\n";
 
-            outFile << "{ \"state\": \"" << state.first << "\" , \"count\": " << state.second <<"}";
-        }
-        outFile << "\n  ]";
-    }
-    outFile << "\n}\n";
-    
+		for (auto state : group.second) {
+			if (!firstEntry) { outFile << ",\n"; } //Used to seperate entries
+			firstEntry = false;
+
+			outFile << "{ \"state\": \"" << state.first << "\" , \"count\": " << state.second << "}";
+		}
+		outFile << "\n  ]";
+	}
+	outFile << "\n}\n";
+
+}
+
+void writeHashFunction(HashTable table) {
+	ofstream myNewData("../data/sorted_ufo_sightings_Hash.csv");
+	vector<pair<string, int>> Data; //string is state, int is visit count
+
+	for (int i = 0; i < table.getbucketSize(); i++) {
+		for (int j = 0; j < table.getBucket()[i].second.size(); j++) {
+			// if state doesn't exist in states vector, add it to the list
+			auto it = find(Data.begin(), Data.end(), "hi");
+			if (it != Data.end())
+				Data.push_back(make_pair(table.getBucket()[i].second[j].state, 1));
+			else
+				Data[i].second++;
+		}
+	}
+	for (int i = 0; i < Data.size(); i++) {
+		myNewData << Data[i].first << " " << Data[i].second << ",";
+		myNewData << "\n";
+	}
 }
 int main() {
-    vector<AlienEncounters> dataContainer;
-
-    ReadFile(dataContainer);
-    map<string, int> temp = getCountryStats(dataContainer);
+	vector<AlienEncounters> dataContainer;
+	ReadFile(dataContainer);
+	map<string, int> temp = getCountryStats(dataContainer);
 
 	//Testing purposes
-    // for (auto itr : temp) {
-    //     cout << itr.first << " " << itr.second <<endl;
-    // }   
+	// for (auto itr : temp) {
+	//     cout << itr.first << " " << itr.second <<endl;
+	// }
 
-    vector<vector<pair<string, int>>> binnedData = equalFreqBins(temp, 10);
-    writeBins(binnedData);
-    createJSONFile("../data/sorted_ufo_sightings.csv", "ufo_sightings.JSON");
-    return 0;
+	vector<vector<pair<string, int>>> binnedData = equalFreqBins(temp, 10);
+	writeBins(binnedData);
+	createJSONFile("../data/sorted_ufo_sightings_Bin.csv", "ufo_sightings_test.JSON");
+	HashTable table;
+	for (int i = 0; i < dataContainer.size(); i++) {
+		table.insert(dataContainer[i]);
+	}
+	writeHashFunction(table);
+	return 0;
 }
